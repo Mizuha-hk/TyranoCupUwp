6cea51d5c4a31fdcb2d335b8d2028100d8bfbec5
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-09-19 18:17:11 +0900

    [add]: 録音機能の実装

--- TyranoCupUwpApp.Shared/Record.cs
+++ TyranoCupUwpApp.Shared/Record.cs
@@ -12,13 +12,14 @@ using Windows.Storage.Streams;
 
 namespace TyranoCupUwpApp.Shared
 {
-    internal class Record : IRecord
+    public class Record : IRecord
     {
         private MediaCapture _mediaCapture;
         private InMemoryRandomAccessStream _stream;
         public bool IsRecording { get; private set; }
+        public string _fileName;
 
-        public async void StartRecording()
+        public async Task StartRecording()
         {
             if (IsRecording)
             {
@@ -31,21 +32,37 @@ namespace TyranoCupUwpApp.Shared
               };
             _mediaCapture = new MediaCapture();
             await _mediaCapture.InitializeAsync(settings);
+            _stream = new InMemoryRandomAccessStream();
             await _mediaCapture.StartRecordToStreamAsync(
               MediaEncodingProfile.CreateWav(AudioEncodingQuality.Auto), _stream);
             IsRecording = true;
         }
 
-        public async void StopRecording()
+        public async Task<string> StopRecording()
         {
             await _mediaCapture.StopRecordAsync();
             IsRecording = false;
-            SaveAudioToFile();
+            string guid = Guid.NewGuid().ToString();
+            await SaveAudioToFile(guid);
+            return guid;
         }
 
-        private async void SaveAudioToFile()
+        private async Task SaveAudioToFile(string guid)
         {
-            
+            IRandomAccessStream audioStream = _stream.CloneStream();
+            StorageFolder storageFolder = Package.Current.InstalledLocation;
+            string desiredName = guid + ".wav";
+            StorageFile storageFile = await storageFolder.CreateFileAsync(
+              desiredName, CreationCollisionOption.GenerateUniqueName);
+            this._fileName = storageFile.Name;
+            using (IRandomAccessStream fileStream =
+              await storageFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                await RandomAccessStream.CopyAndCloseAsync(
+                  audioStream.GetInputStreamAt(0), fileStream.GetOutputStreamAt(0));
+                await audioStream.FlushAsync();
+                audioStream.Dispose();
+            }
         }
     }
 }

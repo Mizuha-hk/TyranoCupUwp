bd93e4991444201296cc04d48e9ac7f2e52a39cd
harakoo321 <kouseih321@gmail.com>
2023-09-19 16:43:28 +0900

    [add]:録音機能のインターフェースおよびクラスの追加

---
+++ TyranoCupUwpApp.Shared/Record.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TyranoCupUwpApp.Shared.api;
+using Windows.ApplicationModel;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace TyranoCupUwpApp.Shared
+{
+    internal class Record : IRecord
+    {
+        private MediaCapture _mediaCapture;
+        private InMemoryRandomAccessStream _stream;
+        public bool IsRecording { get; private set; }
+
+        public async void StartRecording()
+        {
+            if (IsRecording)
+            {
+                throw new InvalidOperationException("Recording already in progress!");
+            }
+            MediaCaptureInitializationSettings settings =
+              new MediaCaptureInitializationSettings
+              {
+                  StreamingCaptureMode = StreamingCaptureMode.Audio
+              };
+            _mediaCapture = new MediaCapture();
+            await _mediaCapture.InitializeAsync(settings);
+            await _mediaCapture.StartRecordToStreamAsync(
+              MediaEncodingProfile.CreateWav(AudioEncodingQuality.Auto), _stream);
+            IsRecording = true;
+        }
+
+        public async void StopRecording()
+        {
+            await _mediaCapture.StopRecordAsync();
+            IsRecording = false;
+            SaveAudioToFile();
+        }
+
+        private async void SaveAudioToFile()
+        {
+            
+        }
+    }
+}
